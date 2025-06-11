82f77fcc4c5dfacd9bbcaadf35113b52a23716a6
Samuele Medici <samuele.medici@quinck.io>
2025-06-11 08:39:19 +0200

    feat: :construction: add renderer xr conf

--- src/main.ts
+++ src/main.ts
@@ -24,9 +24,12 @@ const getGroundHeight = (position: THREE.Vector3): number | null => {
 
 const scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  powerPreference: "high-performance",
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 1.5));
 renderer.setClearColor(0xffffff);
 document.body.appendChild(renderer.domElement);
 
@@ -37,9 +40,11 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 camera.up.set(0, 0, 1);
-camera.position.set(0, -5, 2.2); // instead of 3
+camera.position.set(0, -5, 2.2);
+
 const minHeight = 2.2; // desired camera height above ground
 const controls = new OrbitControls(camera, renderer.domElement);
+
 controls.enableDamping = false;
 controls.dampingFactor = 0.05;
 controls.target.set(0, 0, 0);
@@ -47,6 +52,7 @@ controls.update();
 
 document.body.appendChild(VRButton.createButton(renderer));
 renderer.xr.enabled = true;
+renderer.xr.setFoveation(0.5);
 
 const lccObject = LCCRender.load(
   {
@@ -56,6 +62,7 @@ const lccObject = LCCRender.load(
     renderLib: THREE,
     canvas: renderer.domElement,
     renderer: renderer,
+    useLoadingEffect: true,
   },
   () => {
     console.log("✅ LCC scene loaded.");
@@ -70,7 +77,12 @@ window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-const CAMERA_SPEED = 0.1;
+let CameraSpeed = 0.1;
+let lastUpdateTime = 0;
+
+let NumFrames = 60;
+const UpdateInterval = 1000 / NumFrames;
+
 let moveForward = false;
 let moveBackward = false;
 let moveLeft = false;
@@ -131,20 +143,28 @@ document.onkeyup = (ev: KeyboardEvent) => {
 };
 
 const updateCamera = () => {
+  const now = performance.now();
+  if (now - lastUpdateTime < UpdateInterval) return;
+  lastUpdateTime = now;
+
+  if (!moveForward && !moveBackward && !moveLeft && !moveRight) {
+    return;
+  }
+
   camera.getWorldDirection(direction);
   const forward = direction.clone().setZ(0).normalize();
   right.crossVectors(forward, up).normalize();
 
   let newPosition = camera.position.clone();
 
-  if (moveForward) newPosition.addScaledVector(forward, CAMERA_SPEED);
+  if (moveForward) newPosition.addScaledVector(forward, CameraSpeed);
   if (moveBackward)
-    newPosition.addScaledVector(forward.clone().negate(), CAMERA_SPEED);
-  if (moveRight) newPosition.addScaledVector(right, CAMERA_SPEED);
+    newPosition.addScaledVector(forward.clone().negate(), CameraSpeed);
+  if (moveRight) newPosition.addScaledVector(right, CameraSpeed);
   if (moveLeft)
-    newPosition.addScaledVector(right.clone().negate(), CAMERA_SPEED);
-  if (moveUp) newPosition.add(new THREE.Vector3(0, 0, CAMERA_SPEED));
-  if (moveDown) newPosition.add(new THREE.Vector3(0, 0, -CAMERA_SPEED));
+    newPosition.addScaledVector(right.clone().negate(), CameraSpeed);
+  if (moveUp) newPosition.add(new THREE.Vector3(0, 0, CameraSpeed));
+  if (moveDown) newPosition.add(new THREE.Vector3(0, 0, -CameraSpeed));
 
   const groundHeight = getGroundHeight(newPosition);
   if (groundHeight !== null) {
@@ -175,6 +195,54 @@ const updateCamera = () => {
     }
   }
 };
+
+// renderer.xr.addEventListener("sessionstart", () => {
+//   const desiredPosition = camera.position.clone();
+
+//   const offsetPosition = new THREE.Vector3(
+//     desiredPosition.x,
+//     desiredPosition.y,
+//     desiredPosition.z
+//   );
+
+//   const transform = new THREE.Matrix4().makeTranslation(
+//     offsetPosition.x,
+//     offsetPosition.y,
+//     offsetPosition.z
+//   );
+//   const referenceSpace = renderer.xr.getReferenceSpace();
+//   if (referenceSpace) {
+//     renderer.xr.setReferenceSpaceType("local-floor");
+//   }
+// });
+
+renderer.xr.addEventListener("sessionstart", () => {
+  NumFrames = 30;
+  CameraSpeed = 0.05;
+  const session = renderer.xr.getSession();
+
+  if (session) {
+    const gl = renderer.getContext();
+    const layer = session.renderState.baseLayer;
+    if (layer) {
+      session.updateRenderState({
+        baseLayer: new XRWebGLLayer(session, gl, {
+          framebufferScaleFactor: 0.7,
+        }),
+      });
+    }
+  }
+
+  const offset = camera.position.clone();
+  scene.position.set(-offset.x, -offset.y, -offset.z);
+
+  controls.enabled = false;
+});
+
+renderer.xr.addEventListener("sessionend", () => {
+  scene.position.set(0, 0, 0);
+  controls.enabled = true;
+});
 
 const render = () => {
   updateCamera();
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    server: {
+        allowedHosts: ['cdf7-79-61-135-187.ngrok-free.app']
+    }
+})
