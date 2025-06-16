ac3da3650415be14468ff044feb262b1db174127
Samuele Medici <samuele.medici@quinck.io>
2025-06-16 12:37:45 +0200

    fix: update viteconfi

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     server: {
-        allowedHosts: ['cdf7-79-61-135-187.ngrok-free.app'],
+        allowedHosts: ['quinckdev.com'],
         headers: {
             'Cross-Origin-Embedder-Policy': 'require-corp',
             'Cross-Origin-Opener-Policy': 'same-origin',
